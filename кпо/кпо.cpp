75146643b3daa252cf8e2e8bb4c7df3b1993121d
Edinorogeck <goncharov.1998@mail.ru>
2023-07-13 14:31:34 +0300

    ver. 1.2

--- кпо/кпо.cpp
+++ кпо/кпо.cpp
@@ -5,7 +5,6 @@
 #include "config.h"
 #include "Message.h"
 #include "Parameter.h"
-#include "conv_functions.cpp"
 #include <bitset>
 #include <ostream>
 
@@ -46,32 +45,21 @@ bool Is_Incoming(string name) {
 
 int main()
 {
-
     vector<BYTE> dataByteArray = { 77, 88, 99, 110, 120, 130, 140 };
     string dataString = "MXcnx'?";
 
-  
     Configuration();
     
-
     string messageName = "Вх.МКИО.766";
 
-    
     MessagePrototype mp;
     mp = GetMessagePrototype(messageName);
-
-
-    
-
-
 
     Message VhMs = Message(mp);
     Message IshMs = Message(mp);
     
-    
     VhMs.SetValues(dataByteArray);
     IshMs.SetValues(dataString);
-    
     
     cout << VhMs.MakeOutputString() << endl;
     cout << "-----------------------------" << endl;
@@ -84,17 +72,4 @@ int main()
     }
 
     cout << endl << Address[0];
-       
-
-   /* cout << mp.type << endl;
-    cout << ms.GetAddressOY() << endl;*/
-
-   
-    
-
-    
-
-
-
-
 }
